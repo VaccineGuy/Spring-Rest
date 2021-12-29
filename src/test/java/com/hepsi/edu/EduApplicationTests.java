681ba4c33804993f6e88fd3a7abde3aa54a55eee
VaccineGuy <bogachan.guleryuz@hotmail.com>
2021-12-30 02:25:20 +0300

    First commit spring intilzr

---
+++ src/test/java/com/hepsi/edu/EduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hepsi.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
